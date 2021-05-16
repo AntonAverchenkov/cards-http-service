12d8a7e12ca44fc1807267701d3d086ca219e8b3
Anton Averchenkov <84287187+AntonAverchenkov@users.noreply.github.com>
2021-05-16 19:07:00 -0400

    Add main & handlers (#5)

---
+++ handlers.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"net/http"
+	"sync"
+
+	"github.com/labstack/echo/v4"
+
+	"cards-service/internal/api"
+	"cards-service/internal/game"
+)
+
+//go:embed doc/index.html
+var docIndexHtml string
+
+type handlers struct {
+	lock sync.Mutex
+	deck *game.Deck
+}
+
+// (GET /) index.html that describes this api
+func (h *handlers) Index(ctx echo.Context) error {
+	return ctx.HTML(http.StatusOK, docIndexHtml)
+}
+
+// (GET /cards) Get the current state of the deck
+func (h *handlers) DeckShow(ctx echo.Context) error {
+	h.lock.Lock()
+	defer h.lock.Unlock()
+
+	return ctx.JSONPretty(http.StatusOK, fromGameCards(h.deck.Cards), "  ")
+}
+
+// (POST /cards/shuffle) Permute the deck in an unbiased way
+func (h *handlers) DeckShuffle(ctx echo.Context) error {
+	h.lock.Lock()
+	defer h.lock.Unlock()
+
+	h.deck.Shuffle()
+
+	return ctx.JSONPretty(http.StatusOK, fromGameCards(h.deck.Cards), "  ")
+}
+
+// (GET /cards/shuffle) Permute the deck in an unbiased way
+func (h *handlers) DeckShuffle2(ctx echo.Context) error {
+	return h.DeckShuffle(ctx)
+}
+
+// (POST /cards/deal) Deals the top card by removing it from the deck
+func (h *handlers) DeckDealCard(ctx echo.Context) error {
+	h.lock.Lock()
+	defer h.lock.Unlock()
+
+	card, err := h.deck.DealCard()
+	if err != nil {
+		return ctx.JSONPretty(http.StatusConflict, api.Error{Message: err.Error()}, "  ")
+	}
+
+	return ctx.JSONPretty(http.StatusOK, fromGameCard(card), "  ")
+}
+
+// (GET /cards/deal) Deals the top card by removing it from the deck
+func (h *handlers) DeckDealCard2(ctx echo.Context) error {
+	return h.DeckDealCard(ctx)
+}
+
+// (POST /cards/return) Return the card specified in body to the back of the deck
+func (h *handlers) DeckReturnCard(ctx echo.Context) error {
+	// We expect an api.Card object in the request body
+	var c api.Card
+	err := ctx.Bind(&c)
+	if err != nil {
+		return ctx.JSONPretty(http.StatusBadRequest, api.Error{Message: err.Error()}, "  ")
+	}
+
+	card, err := toGameCard(c)
+	if err != nil {
+		return ctx.JSONPretty(http.StatusBadRequest, api.Error{Message: err.Error()}, "  ")
+	}
+
+	h.lock.Lock()
+	defer h.lock.Unlock()
+
+	err = h.deck.ReturnCard(card)
+	if err != nil {
+		return ctx.JSONPretty(http.StatusConflict, api.Error{Message: err.Error()}, "  ")
+	}
+
+	return ctx.JSONPretty(http.StatusOK, fromGameCards(h.deck.Cards), "  ")
+}
+
+// (GET /cards/return?card={card}) Return the card specified in url parameter to the back of the deck
+func (h *handlers) DeckReturnCard2(ctx echo.Context, params api.DeckReturnCard2Params) error {
+	if params.Card == nil {
+		return ctx.JSONPretty(http.StatusBadRequest, api.Error{Message: "the required url parameter 'card' is missing"}, "  ")
+	}
+
+	card, err := game.ParseCard(*params.Card)
+	if err != nil {
+		return ctx.JSONPretty(http.StatusBadRequest, api.Error{Message: err.Error()}, "  ")
+	}
+
+	h.lock.Lock()
+	defer h.lock.Unlock()
+
+	err = h.deck.ReturnCard(card)
+	if err != nil {
+		return ctx.JSONPretty(http.StatusConflict, api.Error{Message: err.Error()}, "  ")
+	}
+
+	return ctx.JSONPretty(http.StatusOK, fromGameCards(h.deck.Cards), "  ")
+}
+
+///
+/// The translation helpers below are the unfortunate side effect of dealing with swagger middle layer
+///
+
+func fromGameCard(card game.Card) api.Card {
+	return api.Card{
+		Value: card.Value.String(),
+		Suit:  card.Suit.String(),
+	}
+}
+
+func fromGameCards(cards []game.Card) []api.Card {
+	var result []api.Card
+
+	for _, card := range cards {
+		result = append(result, fromGameCard(card))
+	}
+
+	return result
+}
+
+func toGameCard(card api.Card) (game.Card, error) {
+	v, err := game.ParseValue(card.Value)
+	if err != nil {
+		return game.Card{}, fmt.Errorf("error parsing %v: %w", card, err)
+	}
+
+	s, err := game.ParseSuit(card.Suit)
+	if err != nil {
+		return game.Card{}, fmt.Errorf("error parsing %v: %w", card, err)
+	}
+
+	return game.Card{Value: v, Suit: s}, nil
+}
---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/deepmap/oapi-codegen/pkg/middleware"
+	"github.com/jessevdk/go-flags"
+	"github.com/labstack/echo/v4"
+
+	"cards-service/internal/api"
+	"cards-service/internal/game"
+)
+
+type CommandLineOptions struct {
+	Address string `long:"address"  env:"ADDRESS"  description:"Listen to http traffic on this tcp address"   default:"localhost:8080"`
+}
+
+func main() {
+	var cl CommandLineOptions
+
+	_, err := flags.Parse(&cl)
+	if err != nil {
+		if e, ok := err.(*flags.Error); ok && e.Type == flags.ErrHelp {
+			os.Exit(0)
+		}
+		log.Fatalf("Error :: command-line argument parsing failed: %v\n", err)
+	}
+
+	if err := run(cl); err != nil {
+		log.Fatalf("Error :: %v\n", err)
+	}
+}
+
+func run(cl CommandLineOptions) (errs error) {
+	/* */ log.Printf("run(): cards-http-service begin")
+	defer log.Printf("run(): cards-http-service end")
+
+	swagger, err := api.GetSwagger()
+	if err != nil {
+		return fmt.Errorf("could not load swagger spec: %w", err)
+	}
+
+	handlers := handlers{
+		deck: game.NewDeck(),
+	}
+
+	server := echo.New()
+	server.Use(middleware.OapiRequestValidator(swagger))
+
+	api.RegisterHandlers(server, &handlers)
+
+	log.Printf("run(): starting to listen & serve on %q\n", cl.Address)
+
+	if err := server.Start(cl.Address); err != nil {
+		return fmt.Errorf("could not start the server @ %q: %w", cl.Address, err)
+	}
+
+	return nil
+}
