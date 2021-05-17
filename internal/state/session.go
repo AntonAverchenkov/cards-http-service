7d858669750398425f5e7b92aaf24973c281f490
Anton Averchenkov <averchenkov@pm.me>
2021-05-16 21:29:18 -0400

    comment

--- internal/state/session.go
+++ internal/state/session.go
@@ -2,7 +2,7 @@ package state
 
 import "github.com/AntonAverchenkov/cards-http-service/internal/game"
 
-// Session represents a persistent connection with a client
+// Session represents a persistent connection with a client, each client will get their own deck
 type Session struct {
 	Id   string
 	Deck *game.Deck
